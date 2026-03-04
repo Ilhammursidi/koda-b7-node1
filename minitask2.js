c5fb74c753098f36db63f8bb15820432e9ac2b67
Ilhammursidi <imursidi37@gmail.com>
2026-03-04 09:13:41 +0700

    Update minitask2.js

--- minitask2.js
+++ minitask2.js
@@ -1,20 +1,36 @@
-import readline from "readline";
+import * as readline from "node:readline/promises";
+import { stdin as input, stdout as output } from "node:process";
 
-export function dateConvert(input) {
-    const regex =/^\d{2}-\d{2}-\d{4}$/;
-    if (!regex.test(input)) {
-        return "format tanggal salah";
+export async function askDate() {
+  const redline = readline.createInterface({ input, output });
+  try {
+    const hasil = await redline.question(
+      "Masukkan tanggal part2 (DD-MM-YYYY): ",
+    );
+    const regex = /^\d{2}-\d{2}-\d{4}$/;
+    if (!regex.test(hasil)) {
+      console.log("format tanggal salah");
     }
 
-    const [day, month, year] = input.split("-");
+    const [day, month, year] = hasil.split("-");
 
     const dayNum = Number(day);
     const monthNum = Number(month);
     const yearNum = Number(year);
 
-    if(dayNum < 1 || dayNum > 31 || monthNum < 1 || monthNum > 12 || yearNum < 1000 || yearNum > 9999) {
-        console.log("format tanggal salah");
-        return;
-    };
-    return `setelah ganti format ${day}/${month}/${year}`;
-};
+    if (
+      dayNum < 1 ||
+      dayNum > 31 ||
+      monthNum < 1 ||
+      monthNum > 12 ||
+      yearNum < 1000 ||
+      yearNum > 9999
+    ) {
+      console.log("input tanggal salah");
+      return;
+    }
+    console.log(hasil.split("-").join("/"));
+  } finally {
+    redline.close();
+  }
+}
